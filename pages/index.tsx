f023fb5047090de36dc046fc53e7a96ec6aff63d
Zhu's Family <zhusfamily@Zhuss-iMac-Pro.local>
2023-01-05 00:29:34 -0800

    try

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,8 @@
 import type { NextPage } from 'next'
-import Header from '../components/header'
+import Header from '../components/Header'
 import Trending from '../components/trending'
 import CMCtable from '../components/cmc-table/cmcTable'
 import SwapCryptoModal from '../components/swapCryptoModal'
-// import Moralis from 'moralis';
-// import express from 'express';
-// import cors from 'cors';
-// import { parseDashboard } from '../auth/parseDashboard';
-// import { parseServer } from '../auth/parseServer';
-// // import { errorHandler } from '../auth/errorHandler';
-// import config from '../auth/config';
-// import { apiRouter } from './apiRouter';
-
-// const app = express();
-
-// Moralis.start({
-//   apiKey: config.MORALIS_API_KEY,
-// });
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(cors());
-
-// app.use(`/${config.SERVER_ENDPOINT}`, parseServer);
-// app.use('/dashboard', parseDashboard);
-// app.use('/api', apiRouter);
-// // app.use(errorHandler);
-
-// app.use(express.static('public'));
-
-// app.listen(config.PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`${config.APP_NAME} is running on port ${config.PORT}`);
-// });
 
 
 const Home: NextPage = () => {
